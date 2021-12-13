47ab7fb4d0211997c997d759cc0f1c42d3244d82
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2021-12-13 19:06:44 -0300

    firts commit

--- Views/resultados.php
+++ Views/resultados.php
@@ -9,7 +9,7 @@ $img = "capas-livros/".$resultado['fotoCapa'];
 <section class="livro">
 	<h2 class="titulo"><?=$resultado['titulo']?></h2>
 	<section class="capa-img">
-		<img src=<?= $img ?> />
+		<img style="width: 200px"src=<?= $img ?> />
 	</section>
 </section>
 
